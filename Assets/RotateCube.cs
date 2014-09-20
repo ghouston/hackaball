206984806214d630b5c0b6011af099c02c015a4d
Greg Houston <ghouston@yahoo.com>
2014-09-20 12:09:48 -0500

    adjusted course so that ball will not stick between objects.  added rotating cube above the goal so that a direct fall is not possible.

---
+++ Assets/RotateCube.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+	public int rotateX;
+	public int rotateY;
+	public int rotateZ;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (rotateX, rotateY, rotateZ) * Time.deltaTime);
+	}
+}
